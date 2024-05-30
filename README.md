e10388c7e984cb73eb74828ab9cc7edb5b76afd3
unknown <artamcrafts@hotmail.com>
2024-05-30 18:16:39 -0300

    Clean up tracked files based on .gitignore

---
+++ README.md
@@ -0,0 +1,18 @@
+CaC_Movies_C24111_G13
+
+TP - FullStack JAVA
+Codo a Codo
+
+> Profesor: Miguel Nefle
+
+> Comisión: 24111
+
+> Grupo 13 - Integrantes:
+- PONCE Claudia
+- CANZOBRE Tamara
+- ZALAZAR GOMEZ Alejandro
+- FERNANDEZ Emanuel
+
+> Deploy: https://tam-s-c.github.io/CaC_Movies_C24111_G13/
+
+> GitHub: https://github.com/Tam-S-C/CaC_Movies_C24111_G13.git
---
+++ validar.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded',() => {
+const formulario = document.querySelector('form');
+// mostrar error
+const mostrarError = (input,mensaje)=>{
+    const divPrimero = input.parentNode;
+    const errorText = divPrimero.querySelector('.error-text');
+    divPrimero.classList.add('error');
+    errorText.innerText = mensaje;
+}
+
+const input = document.querySelector('#email');
+const mensaje = 'campo obligatorio';
+
+//.............................................
+const eliminarError = input =>{
+    const divPrimero = input.parentNode;
+    divPrimero.classList.remove('error');
+    const errorText = divPrimero.querySelector('.error-text');
+    errorText.innerText = ' ';
+}
+
+//...................................
+//funcion para validar que se haya ingresado el dato
+formulario.querySelectorAll('input').forEach(input=> {
+    input.addEventListener('change',()=>{
+        const valor =input.value.trim();
+        if(valor !== ''){
+            eliminarError(input);
+        }
+    })
+});
+
+//...........................................
+// validar datos ingresados
+function validarCampo(campoId,mensaje){
+    const campo = document.getElementById(campoId);
+    const value = campo.value.trim();
+    if(value == ''){
+        mostrarError(campo,mensaje);
+        return false;
+        }else{
+            eliminarError(campo);
+            return true;
+        }
+    }
+    //validar email
+    function isEmail(email){
+        const expresionRegular =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return expresionRegular.test(email);
+    }
+
+    function validarEmail(campoId,mensaje){
+        const campo = document.getElementById(campoId);
+        const email = campo.value.trim();
+        if (email == ' '){
+            mostrarError(campo, 'El correo electronico es obligatorio');
+            return false;
+         }else if(!isEmail(email)){
+            mostrarError(campo, mensaje);
+            return false;
+         }else{
+            eliminarError(campo);
+            return true;
+         }
+    }
+    //...........................
+    //funcion validar el formulario
+    const validarFormulario =() =>{
+        let validar = true;
+        validar = validarEmail('email','El correo electronico no es válido')&& validar;
+        validar = validarCampo('password','La contraseña es obligatoria')&& validar;
+
+        return validar;
+    }
+    //........................
+    // Evento de escucha
+    formulario.addEventListener('submit', event =>{
+        event.preventDefault();
+        if(!validarFormulario()){
+            event.preventDefault();
+            console.log("El formulario no es válido");
+        }else{
+            event.preventDefault();
+            console.log("El formulario es válido.");
+        }
+    })
+})
